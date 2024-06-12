8a95ba691bcdd2fe59bcc6965d8c0a4ad86505ec
John Irungu <johnirungu977@gmail.com>
2024-06-12 13:49:09 +0300

    Part1b CourseInfo Exercise

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,35 +1,35 @@
 import React from "react";
 
 const Header = (props) => {
+  console.log(props);
   return (
     <div>
-      <h1>{props.course}</h1>
-    </div>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.name}: {props.exercises}
-      </p>
+      <h1>{props.course.name}</h1>
     </div>
   );
 };
 
 const Content = (props) => {
+  console.log(props);
   return (
     <div>
-      <Part name={props.name} exercises={props.exercises} />
+      <p>
+        name={props.parts[0].name}: exercises={props.parts[0].exercises}
+      </p>
+      <p>
+        name={props.parts[1].name}: exercises={props.parts[1].exercises}
+      </p>
+      <p>
+        name={props.parts[2].name}: exercises={props.parts[2].exercises}
+      </p>
     </div>
   );
 };
 
 const Total = (props) => {
-  const e1 = props.exercises1;
-  const e2 = props.exercises2;
-  const e3 = props.exercises3;
+  const e1 = props.parts[0].exercises;
+  const e2 = props.parts[1].exercises;
+  const e3 = props.parts[2].exercises;
 
   return (
     <div>
@@ -39,26 +39,30 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content name={part1} exercises={exercises1} />
-      <Content name={part2} exercises={exercises2} />
-      <Content name={part3} exercises={exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
 
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Total parts={course.parts} />
     </div>
   );
 };
--- part1/courseinfo/vite.config.js
+++ part1/courseinfo/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "/Users/irungu/Desktop/React Web Development/part1/courseinfo/",
   plugins: [react()],
-})
+});
